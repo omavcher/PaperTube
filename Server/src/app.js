d9885e8722911bd3d763e1f03c98cc0917e84c33
Om Avcher <omawchar07@gmial.com>
2025-10-15 08:08:05 +0530

    Updated app.js and client UI components

--- Server/src/app.js
+++ Server/src/app.js
@@ -15,7 +15,7 @@ const app = express();
 
 // Middlewares
 app.use(cors(
-    { origin: 'http://localhost:3000', credentials: true }
+    { origin: process.env.CLIENT, credentials: true }
 ));
 app.use(express.json());
 
