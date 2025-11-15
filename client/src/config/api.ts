69ce98302b371921f0fff965553199b57b432af9
Om Avcher <omawchar07@gmial.com>
2025-11-15 20:19:22 +0530

    Finial for the college presentation

--- client/src/config/api.ts
+++ client/src/config/api.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     // baseURL: 'http://localhost:5000/api',
+    baseURL:'https://papertube-j60n.onrender.com/api',
     // baseURL: 'https://papertube.onrender.com/api',
     // baseURL: 'https://papertube-ybzq.onrender.com/api',
     headers: {
