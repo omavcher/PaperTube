4b02e0994a25fd856c28fc3a70ba7f046ebaa2d0
Om Avcher <omawchar07@gmial.com>
2026-01-19 18:14:58 +0530

    fix: the pdflit

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,3 +1,10 @@
+// src/app/not-found.tsx
+import React from 'react';
+
 export default function NotFound() {
-  return <div>404 - Page Not Found</div>;
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-black text-white">
+      <h1 className="text-xl font-bold">404 - Page Not Found</h1>
+    </div>
+  );
 }
