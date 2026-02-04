0c62994ab03a33e6aaa5ab187e9a3d77d880e426
Om Avcher <omawchar07@gmial.com>
2026-02-04 13:40:58 +0530

    just try

--- client/src/components/LoaderX.tsx
+++ client/src/components/LoaderX.tsx
@@ -11,7 +11,6 @@ export function LoaderX() {
         <div className="absolute inset-0 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:40px_40px]"></div>
       </div>
 
-      {/* Rotating Neural Energy Field */}
       <div className="relative flex items-center justify-center">
         <motion.div
           animate={{ rotate: 360 }}
