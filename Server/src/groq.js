07e390db63c7c5b564662e3bd8b61656aa611e4f
Om Avcher <omawchar07@gmial.com>
2026-02-04 13:40:14 +0530

    just try

--- Server/src/groq.js
+++ Server/src/groq.js
@@ -2,7 +2,7 @@ const Groq = require("groq-sdk");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const client = new Groq("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");
+const client = new Groq(process.env.GROQ_API_KEY);
 
 // List of all available free models in priority order
 const FREE_MODELS = [
