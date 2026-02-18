30e0eeb344ce3849528762ed2a7cc338a45e32d7
Om Avcher <omawchar07@gmial.com>
2026-02-19 01:58:50 +0530

    Adding a agent in the youtube-transcript.js

--- Server/src/youtube-transcript.js
+++ Server/src/youtube-transcript.js
@@ -1,10 +1,10 @@
 // youtube-transcript.js
 async function getTranscript(videoId) {
     const { fetchTranscript } = await import('youtube-transcript-plus');
-
+    console.log("js js js js js js js js js js js js js js js js js js")
 
     const transcript = await fetchTranscript(videoId);
-console.log('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+
     // Format the transcript with timestamps
     const formattedTranscript = transcript
         .map(segment => `[${segment.offset.toFixed(1)}s] ${segment.text}`)
--- Server/src/youtube-transcript.mjs
+++ Server/src/youtube-transcript.mjs
@@ -1,13 +1,9 @@
 // youtube-transcript.js
 const { fetchTranscript } = require('youtube-transcript-plus');
 
-const extractVideoId = (url) => {
-    const regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
-    const match = url.match(regExp);
-    return (match && match[2].length === 11) ? match[2] : null;
-};
-
 async function getTranscript(videoId) {
+        console.log("mjs mjs mjsmjs mjs mjsmjs mjs mjsmjs mjs mjsmjs mjs mjsmjs mjs mjsmjs mjs mjsmjs mjs mjsmjs mjs mjs")
+
     const transcript = await fetchTranscript(videoId);
 
     // Format the transcript with timestamps
